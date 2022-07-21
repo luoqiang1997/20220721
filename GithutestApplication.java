468e750da343ec9eee6c606ab18460edc090208b
root <1416167262@qq.com>
2022-07-21 10:20:02 +0800

    测试

---
+++ GithutestApplication.java
@@ -0,0 +1,13 @@
+package com.example.githutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithutestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithutestApplication.class, args);
+    }
+
+}
